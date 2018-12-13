95cffd3b62b9105c09a3b9ce438984e8fcaa3031
tianci <superealboom@163.com>
2018-12-13 10:44:10 +0800

    创建git测试文件

---
+++ src/test/java/com/tianci/GitTest.java
@@ -0,0 +1,11 @@
+package com.tianci;
+
+/**
+ * Create by tianci
+ * 2018/12/13 10:42
+ */
+public class GitTest {
+    public void test() {
+        System.out.println("Git first test use IDEA.");
+    }
+}
