d7eb899b0d3816b478d854cedb015ffd1a5f8693
tianci <superealboom@163.com>
2019-02-12 16:52:56 +0800

    mybatis

--- src/main/java/com/tianci/SellApplication.java
+++ src/main/java/com/tianci/SellApplication.java
@@ -1,9 +1,11 @@
 package com.tianci;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.tianci.dataobject.mapper")
 public class SellApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/tianci/dataobject/mapper/ProductCategoryMapper.java
@@ -0,0 +1,38 @@
+package com.tianci.dataobject.mapper;
+
+import com.tianci.dataobject.ProductCategory;
+import org.apache.ibatis.annotations.*;
+
+import java.util.Map;
+
+/**
+ * Create by tianci
+ * 2019/2/11 15:47
+ */
+public interface ProductCategoryMapper {
+
+    @Insert("insert into product_category(category_name, category_type) values (#{category_name, jdbcType=VARCHAR}, #{category_type, jdbcType=INTEGER})")
+    int insertByMap(Map<String, Object> map);
+
+    @Insert("insert into product_category(category_name, category_type) values (#{categoryName, jdbcType=VARCHAR}, #{categoryType, jdbcType=INTEGER})")
+    int insertByObject(ProductCategory productCategory);
+
+    @Select("select * from product_category where category_type = #{categoryType}")
+    @Results({
+            @Result(column = "category_id", property = "categoryId"),
+            @Result(column = "category_name", property = "categoryName"),
+            @Result(column = "category_type", property = "categoryType")
+    })
+    ProductCategory findByCategoryType(Integer categoryType);
+
+    @Update("update product_category set category_name = #{categoryName} where category_type = #{categoryType}")
+    int updateByCategoryType(@Param("categoryName") String categoryName,
+                             @Param("categoryType") Integer categoryType);
+
+    @Update("update product_category set category_name = #{categoryName} where category_type = #{categoryType}")
+    int updateByObject(ProductCategory productCategory);
+
+    @Delete("delete from product_category where category_type = #{categoryType}")
+    int deleteByCategoryType(Integer categoryType);
+
+}
